87dca6fcac76096661c790ac4257223b1c51d09b
Maureen <morynmoh@gmail.com>
2023-07-01 18:01:30 +0300

    Completed dog and person class

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog
+    def initialize (name, breed="Mutt")
+        @name = name #the name argument is stored within a @name instance variable
+        @breed = breed #the breed argument is stored within a @breed instance variable
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,5 @@
+class Person
+    def initialize (name)
+        @name = name #the name argument is stored within a @name instance variable
+    end
+end
